67a5da6779cc0f415225402b8759a95960097ad4
unknown <kiasat@gmail.com>
2023-06-09 12:57:42 -0700

    initial commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["cdn.imagin.studio"]
+    }
+}
+
+module.exports = nextConfig
